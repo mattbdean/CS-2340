ee6b8403200508e6a85e5a4e54c68bafb83e3795
Matthew Dean <deanmatthew16@gmail.com>
2018-02-20 12:10:27 -0500

    Automatically attempt the login the user presses the "next" button on the soft keyboard

--- app/src/main/java/com/github/alphabet26/LoginActivity.java
+++ app/src/main/java/com/github/alphabet26/LoginActivity.java
@@ -5,8 +5,11 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public final class LoginActivity extends AppCompatActivity {
     private static final String USERNAME = "username";
@@ -22,6 +25,19 @@ public final class LoginActivity extends AppCompatActivity {
 
         this.usernameField = ((TextInputLayout) findViewById(R.id.username)).getEditText();
         this.passwordField = ((TextInputLayout) findViewById(R.id.password)).getEditText();
+
+        // Automatically attempt the login the user presses the "next" button on the soft keyboard
+        passwordField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    onLogin(null);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
     public void onLogin(View view) {
