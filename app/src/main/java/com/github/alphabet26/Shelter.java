662582d5bfe6ce96fca91d3a07227a5f27bcf509
jlefelhocz <jlefelhocz@yahoo.com>
2018-02-27 12:07:13 -0500

    Implement Create an Account button to link to RegisterActivity

---
+++ app/src/main/java/com/github/alphabet26/Shelter.java
@@ -0,0 +1,20 @@
+package com.github.alphabet26;
+
+/**
+ * Shelter is a representation of a homeless shelter in the real world, it mainly
+ * holds information
+ * Created by Julianne Lefelhocz on 2/27/18.
+ */
+
+public class Shelter {
+    private String shelterName;
+    private int capacity;
+    private int openBeds;
+    private float longitude;
+    private float latitude;
+    private String address;
+    private String phoneNumber;
+    private Gender gender;
+
+
+}
