8ec74a6ca2bc68c2e36a0d31c8b735383f9856ca
Matthew Dean <deanmatthew16@gmail.com>
2018-02-27 13:01:28 -0500

    Add ShelterDao and accessors in App

--- app/src/main/java/com/github/alphabet26/App.java
+++ app/src/main/java/com/github/alphabet26/App.java
@@ -3,10 +3,15 @@ package com.github.alphabet26;
 import android.app.Application;
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App extends Application {
     private static App instance;
 
     private UserDao userDao;
+    private ShelterDao shelterDao;
+
     private User activeUser;
 
     @Override
@@ -17,13 +22,21 @@ public class App extends Application {
         if (BuildConfig.DEBUG) {
             this.userDao = new InMemoryUserDao();
             this.userDao.register(new UserRegistrationInfo("<testing user>", "username", "password", UserType.USER));
+
+            List<Shelter> tempShelters = new ArrayList<>();
+            tempShelters.add(new Shelter("Test", 5, 2, 100, 100, "addy", "test", Gender.MALE));
+            tempShelters.add(new Shelter("Test2", 10, 5, 100, 100, "addy", "test", Gender.FEMALE));
+
+            this.shelterDao = new InMemoryShelterDao(tempShelters);
         } else {
             // TODO replace with other implementation such as Firebase or Android's SQLite db
             this.userDao = new InMemoryUserDao();
+            this.shelterDao = new InMemoryShelterDao();
         }
     }
 
     @NonNull public UserDao getUserDao() { return userDao; }
+    @NonNull public ShelterDao getShelterDao() { return shelterDao; }
 
     public User getActiveUser() {
         if (activeUser == null) {
@@ -45,4 +58,9 @@ public class App extends Application {
     public void onLogout() { this.activeUser = null; }
 
     @NonNull public static App get() { return instance; }
+
+    private static List<Shelter> loadFromCsv(String filename) {
+        // TODO
+        return new ArrayList<>();
+    }
 }
