2bf66cfe6eff48f556ee9923675d99ab0a7ebc21
Matthew Dean <deanmatthew16@gmail.com>
2018-02-27 12:27:58 -0500

    Make Shelter immutable, add getters and constructor

--- app/src/main/java/com/github/alphabet26/Shelter.java
+++ app/src/main/java/com/github/alphabet26/Shelter.java
@@ -5,16 +5,35 @@ package com.github.alphabet26;
  * holds information
  * Created by Julianne Lefelhocz on 2/27/18.
  */
+public final class Shelter {
+    private final String shelterName;
+    private final int capacity;
+    private final int openBeds;
+    private final float longitude;
+    private final float latitude;
+    private final String address;
+    private final String phoneNumber;
+    private final Gender gender;
 
-public class Shelter {
-    private String shelterName;
-    private int capacity;
-    private int openBeds;
-    private float longitude;
-    private float latitude;
-    private String address;
-    private String phoneNumber;
-    private Gender gender;
+    public Shelter(String shelterName, int capacity, int openBeds, float longitude, float latitude,
+                   String address, String phoneNumber, Gender gender) {
 
+        this.shelterName = shelterName;
+        this.capacity = capacity;
+        this.openBeds = openBeds;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
 
+    public String getName() { return shelterName; }
+    public int getCapacity() { return capacity; }
+    public int getOpenBeds() { return openBeds; }
+    public float getLongitude() { return longitude; }
+    public float getLatitude() { return latitude; }
+    public String getAddress() { return address; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public Gender getGender() { return gender; }
 }
