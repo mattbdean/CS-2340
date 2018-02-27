c5965ae7ea303b10b28727f74940e37595f311e4
jlefelhocz <jlefelhocz@yahoo.com>
2018-02-27 12:20:07 -0500

    Add gender enum

---
+++ app/src/main/java/com/github/alphabet26/Gender.java
@@ -0,0 +1,11 @@
+package com.github.alphabet26;
+
+/**
+ * Stores gender in a not socially acceptable way
+ * Created by julianne lefelhocz on 2/27/18.
+ */
+
+public enum Gender {
+    FEMALE,
+    MALE
+}
