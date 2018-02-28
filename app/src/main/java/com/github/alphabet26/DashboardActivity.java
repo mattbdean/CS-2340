d3facb8863876a2e95268bf25aa62d662e850b3a
Govinda Harris <gharris108@gatech.edu>
2018-02-28 00:50:13 -0500

    Updated DashboardActivity to use Recycler view, connects to in memory shelter dao Created RecyclerAdapter, creates views as necessary Updated activity_dashboard.xml with necessary recycler view code Created shelter_list_row.xml, represents single entry in recycler view list Updated build.gradle, added recyclerView dependency

--- app/src/main/java/com/github/alphabet26/DashboardActivity.java
+++ app/src/main/java/com/github/alphabet26/DashboardActivity.java
@@ -3,34 +3,27 @@ package com.github.alphabet26;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import java.util.ArrayList;
-import android.widget.TextView;
 
 public final class DashboardActivity extends AppCompatActivity {
 
-    private ListView sheltersListView;
+    private RecyclerView sheltersRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        sheltersListView = (ListView) findViewById(R.id.shelters_list_view);
-        ArrayList<String> shelters = new ArrayList<>();
-        shelters.add("Shelter1");
-        shelters.add("Shelter2");
-        shelters.add("Shelter3");
+        sheltersRecyclerView = (RecyclerView) findViewById(R.id.shelters_recycler_view);
 
-        String[] listItems = new String[shelters.size()];
-// 3
-        for(int i = 0; i < shelters.size(); i++){
-            listItems[i] = shelters.get(i);
-        }
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        sheltersListView.setAdapter(adapter);
+        mLayoutManager = new LinearLayoutManager(this);
+        sheltersRecyclerView.setLayoutManager(mLayoutManager);
 
+        mAdapter = new RecyclerAdapter(App.get().getShelterDao().find());
+        sheltersRecyclerView.setAdapter(mAdapter);
     }
 
     public void onLogout(@Nullable View view) {
---
+++ app/src/main/java/com/github/alphabet26/RecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.github.alphabet26;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Adapter for recycler view. Creates view for objects as necessary.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private List<Shelter> mShelterList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mShelterName, mShelterGender, mShelterPhone;
+
+        public ViewHolder(View v) {
+            super(v);
+            mShelterName = (TextView) itemView.findViewById(R.id.shelter_name);
+            mShelterGender = (TextView) itemView.findViewById(R.id.shelter_gender);
+            mShelterPhone = (TextView) itemView.findViewById(R.id.shelter_phone);
+        }
+    }
+
+    // adapter constructor
+    public RecyclerAdapter(List<Shelter> shelterList) {
+        mShelterList = shelterList;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.shelter_list_row, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Shelter shelter = mShelterList.get(position);
+        holder.mShelterName.setText(shelter.getName());
+        holder.mShelterGender.setText(shelter.getGender().name());
+        holder.mShelterPhone.setText(shelter.getPhoneNumber());
+    }
+
+    // Return the size of list (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mShelterList.size();
+    }
+}
